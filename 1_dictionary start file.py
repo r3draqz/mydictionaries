8495a53d7ebcd195237ed7404154f168a7b813e9
r3draqz <raquael_williams1@baylor.edu>
2023-01-30 14:15:14 -0600

    dictionary 01-30

--- 1_dictionary start file.py
+++ 1_dictionary start file.py
@@ -1,17 +1,21 @@
 import random
 
-phonebook = {'Chris':'555−1111',
-             'Katie':'555−2222',
-             'Joanne':'555−3333'}
+phonebook = {}
+phonebook = {"Chris": "555−1111", "Katie": "555−2222", "Joanne": "555−3333"}
 
+mydictionary = dict(m=8, n=9)
 
+print(mydictionary)
 
+print(f"Number of key-value pairs: {len(phonebook)}")
+
+"""
 print()
 print('*****  start section 1 - print dictionary ********')
 print()
 
 
-
+print(phonebook)
 
 
 print()
@@ -19,13 +23,19 @@ print('*****  end section 1 ********')
 print()
 
 
-'''
+
 
 
 print()
 print('*****  start section 2 - search dictionary ********')
 print()
 
+name = 'chris'
+
+if name in phonebook:
+    print(phonebook['Chris'])
+else:
+    print(f"{name} doesn't exist in the phonebook")
 
 
 
@@ -46,7 +56,13 @@ print()
 print('*****  start section 3 - edit/append dictionary ********')
 print()
 
+print(phonebook)
 
+phonebook['Chris'] = '555-4444'
+
+phonebook['Joe'] = '555-0123'
+
+print(phonebook)
 
 
 
@@ -63,8 +79,9 @@ print()
 print('*****  start section 4 - delete/remove from dictionary ********')
 print()
 
-
-
+print(phonebook)
+del phonebook['Chris']
+print(phonebook)
 
 print()
 print('*****  end section 4 ********')
@@ -76,83 +93,91 @@ print()
 
 
 print()
-print('*****  start section 5 - iterate through keys, values, items ********')
+print("*****  start section 5 - iterate through keys, values, items ********")
 print()
 
+for key in phonebook:
+    print(f"The key is: {key} and the value is {phonebook[key]}")
 
+for value in phonebook.values():
+    print(value)
 
+for k, v in phonebook.items():
+    print(f"The key is: {k} and the value is {v}")
 
-
-
-print()
-print('*****  end section 5 ********')
-print()
-
-
-
-
-
-print()
-print('*****  start section 6 - using get and clear ********')
-print()
-
-
-
-
+for ph_tuple in phonebook.items():
+    print(ph_tuple)
 
 
 print()
-print('*****  end section 6 ********')
+print("*****  end section 5 ********")
 print()
 
 
 
 print()
-print('*****  start section 7 - using pop method ********')
+print("*****  start section 6 - using get and clear ********")
 print()
 
 
-
-
-
-
-print()
-print('*****  end section 7 ********')
-print()
-
+name = "chris"
+phone = phonebook.get(name, "555-9999")
+print(phone)
+phonebook.clear()
+print(phonebook)
 
 
 print()
-print('*****  start section 8 - using popitem ********')
-print()
-
-
-
-
-
-
-print()
-print('*****  end section 8 ********')
+print("*****  end section 6 ********")
 print()
 
 
 
 print()
-print('*****  start section 9 - using random and converting to list ********')
+print("*****  start section 7 - using pop method ********")
+print()
+
+
+remove = phonebook.pop("Chris", "not found")
+print(remove)
+print(phonebook)
+
+print()
+print("*****  end section 7 ********")
 print()
 
 
 
-
-
 print()
-print('*****  end section 9 ********')
+print("*****  start section 8 - using popitem ********")
 print()
 
 
-'''
+a = phonebook.popitem()
+
+print(a)
+
+print(phonebook)
 
 
+print()
+print("*****  end section 8 ********")
+print()
 
 
+"""
+print()
+print("*****  start section 9 - using random and converting to list ********")
+print()
 
+
+list_of_keys = list(phonebook)
+random_key = random.choice(list_of_keys)
+print(random_key)
+print(phonebook[random_key])
+
+print(phonebook[random.choice(list(phonebook))])
+
+print()
+print("*****  end section 9 ********")
+print()
--- 2_person_dict.py
+++ 2_person_dict.py
@@ -5,3 +5,24 @@ person["age"] = 51
 person["spouse"] = "Edna"
 person["children"] = ["Ralph", "Betty", "Joey"]
 person["pets"] = {"dog": "Fido", "cat": "Sox"}
+
+print(person)
+
+# name of second child
+print(type(person["children"]))
+print(person["children"][1])
+
+
+# name of cat
+print(type(person["pets"]["cat"]))
+print(person["pets"]["cat"])
+
+
+# use for loop to list each child
+for child in person["children"]:
+    print(child)
+
+# print out the pets in this format;
+# 'type of pet: dog name of pet: Fido'
+for pet, name in person["pets"].items():
+    print(f"Type of pet: {pet} name of pet: {name}")
