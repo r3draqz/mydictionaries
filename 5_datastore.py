ae609b3adc68dca4750880ded04051170c5c1060
r3draqz <raquael_williams1@baylor.edu>
2023-02-01 14:04:30 -0600

    feb 1 classwork dictionary exercises

--- 5_datastore.py
+++ 5_datastore.py
@@ -3,7 +3,7 @@
 # for each space represented as rows. Name your file 'retail_space.csv.
 
 
-'''
+"""
 Your final output should look like:
 
 room-number,use,sq-ft,price
@@ -13,37 +13,32 @@ room-number,use,sq-ft,price
 103,examination,125,150
 104,office,150,100
 
-'''
+"""
+import csv
 
 
-
-
-datastore = { "medical":[
-      { "room-number": 100,
-        "use": "reception",
-        "sq-ft": 50,
-        "price": 75
-      },
-      { "room-number": 101,
-        "use": "waiting",
-        "sq-ft": 250,
-        "price": 75
-      },
-      { "room-number": 102,
-        "use": "examination",
-        "sq-ft": 125,
-        "price": 150
-      },
-      { "room-number": 103,
-        "use": "examination",
-        "sq-ft": 125,
-        "price": 150
-      },
-      { "room-number": 104,
-        "use": "office",
-        "sq-ft": 150,
-        "price": 100
-      }
-
-      ]
+datastore = {
+    "medical": [
+        {"room-number": 100, "use": "reception", "sq-ft": 50, "price": 75},
+        {"room-number": 101, "use": "waiting", "sq-ft": 250, "price": 75},
+        {"room-number": 102, "use": "examination", "sq-ft": 125, "price": 150},
+        {"room-number": 103, "use": "examination", "sq-ft": 125, "price": 150},
+        {"room-number": 104, "use": "office", "sq-ft": 150, "price": 100},
+    ]
 }
+
+outfile = open("retail_space.csv", "w", newline="")
+outfile.write("room-number, use, sq-ft, price\n")
+
+list1 = datastore["medical"]
+for dict in list1:
+    room_number = dict["room-number"]
+    use = dict["use"]
+    sq = dict["sq-ft"]
+    price = dict["price"]
+
+    outfile.write(
+        str(room_number) + "," + use + "," + str(sq) + "," + str(price) + "\n"
+    )
+
+outfile.close()
